98328829628725f969034bccce59a0c3cc8c6b07
Graeme Nathan <graemenathan@gmail.com>
2022-05-10 18:56:47 -0700

    Find leaf for insertion

--- trees/two-three.go
+++ trees/two-three.go
@@ -1,10 +1,13 @@
 package trees
 
-/* TwoThreeTree is a binary search tree with the following properties:
- * 1. The left subtree of a node contains only nodes with keys less than the node's key.
- * 2. The right subtree of a node contains only nodes with keys greater than the node's key.
- * 3. The left and right subtrees of a node do not contain duplicate keys.
- */
+import (
+	"errors"
+)
+
+/* TwoThreeTree is a binary search tree where each node has either:
+    - two children (aka 2-node) and one data element, or
+	- three children (aka 3-node) and two data elements
+*/
 
 type twoThreeNode[T any] struct {
 	//Each node can have a maximum of two data values.
@@ -15,22 +18,106 @@ type twoThreeNode[T any] struct {
 
 	//Each node has a parent, except for the root node.
 	parent *twoThreeNode[T]
+
+	// comparator is used to compare two values.
+	// returns -1 if a < b, 0 if a == b, 1 if a > b
+	comparator func(T, T) int
 }
 
-/**
-Returns a root node of a two-three tree.
-*/
-func TwoThreeNode[T any](init *T) *twoThreeNode[T] {
-	return &twoThreeNode[T]{
+// TwoThreeNodeInt is a constructor for a two-three tree with int values.
+// It returns a root node of a two-three tree.
+func TwoThreeNodeInt(init *int) *twoThreeNode[int] {
+	return &twoThreeNode[int]{
 		firstData:   init,
 		secondData:  nil,
 		firstChild:  nil,
 		secondChild: nil,
 		thirdChild:  nil,
 		parent:      nil,
+		comparator:  intComparator,
+	}
+}
+
+func intComparator(a, b int) int {
+	if a < b {
+		return -1
+	} else if a == b {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+func stringComparator(a, b string) int {
+	if a < b {
+		return -1
+	} else if a == b {
+		return 0
+	} else {
+		return 1
 	}
 }
 
 func isLeaf[T any](node twoThreeNode[T]) bool {
 	return node.firstChild == nil && node.secondChild == nil && node.thirdChild == nil
+}
+
+const (
+	twoNode = iota + 2
+	threeNode
+)
+
+func nodeType[T any](node twoThreeNode[T]) (int, error) {
+	var zeroVal int
+	firstChild := node.firstChild != nil
+	secondChild := node.secondChild != nil
+	thirdChild := node.thirdChild != nil
+	firstDatum := node.firstData != nil
+	secondDatum := node.secondData != nil
+
+	if isLeaf(node) {
+		return zeroVal, errors.New("node is a leaf")
+	}
+
+	if firstChild && secondChild && !thirdChild && firstDatum && !secondDatum {
+		return twoNode, nil
+	} else if firstChild && secondChild && thirdChild && firstDatum && secondDatum {
+		return threeNode, nil
+	} else {
+		return zeroVal, errors.New("node is not a valid two-three tree node")
+	}
+}
+
+// findLeaf locates the leaf node of the given twoThreeTree within which the value should be inserted.
+// It returns the leaf node at which the value should be inserted.
+// If no leaf node exists, it returns nil.
+func findLeaf[T any](node twoThreeNode[T], value T) (*twoThreeNode[T], error) {
+	if isLeaf(node) {
+		return &node, nil
+	}
+
+	nt, err := nodeType(node)
+
+	if err != nil {
+		return nil, err
+	}
+
+	switch nt {
+	case twoNode:
+		if node.comparator(value, *node.firstData) < 0 {
+			return findLeaf(*node.firstChild, value)
+		} else {
+			return findLeaf(*node.secondChild, value)
+		}
+	case threeNode:
+		if node.comparator(value, *node.firstData) < 0 {
+			return findLeaf(*node.firstChild, value)
+		} else if node.comparator(value, *node.secondData) < 0 {
+			return findLeaf(*node.secondChild, value)
+		} else {
+			return findLeaf(*node.thirdChild, value)
+		}
+	}
+
+	return nil, errors.New("Unknown node type")
 }
--- trees/two-three_test.go
+++ trees/two-three_test.go
@@ -1,7 +1,7 @@
 package trees
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 )
 
@@ -27,13 +27,18 @@ func TestTwoThreeNode(t *testing.T) {
 				secondChild: nil,
 				thirdChild:  nil,
 				parent:      nil,
+				comparator:  intComparator,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := TwoThreeNode(tt.args.init); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TwoThreeNode() = %v, want %v", got, tt.want)
+			got := TwoThreeNodeInt(tt.args.init)
+			if got.firstData != tt.want.firstData {
+				t.Errorf("TwoThreeNodeInt() = %v, want %v", got.firstData, tt.want.firstData)
+			}
+			if comparison := got.comparator(*got.firstData, *tt.want.firstData); comparison != 0 {
+				t.Errorf("TwoThreeNodeInt.comparator(x, x) = %v, want 0", comparison)
 			}
 		})
 	}
@@ -58,6 +63,7 @@ func Test_isLeaf(t *testing.T) {
 					secondChild: nil,
 					thirdChild:  nil,
 					parent:      nil,
+					comparator:  stringComparator,
 				},
 			},
 			want: true,
@@ -81,6 +87,180 @@ func Test_isLeaf(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isLeaf(tt.args.node); got != tt.want {
 				t.Errorf("isLeaf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_nodeType(t *testing.T) {
+	type args struct {
+		node twoThreeNode[int]
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "Returns 3 when the node has three children and two datum.",
+			args: args{
+				node: twoThreeNode[int]{
+					firstData:   new(int),
+					secondData:  new(int),
+					firstChild:  &twoThreeNode[int]{},
+					secondChild: &twoThreeNode[int]{},
+					thirdChild:  &twoThreeNode[int]{},
+				},
+			},
+			want:    threeNode,
+			wantErr: false,
+		},
+		{
+			name: "Returns 2 when the node has two children and one datum.",
+			args: args{
+				node: twoThreeNode[int]{
+					firstData:   new(int),
+					secondData:  nil,
+					firstChild:  &twoThreeNode[int]{},
+					secondChild: &twoThreeNode[int]{},
+					thirdChild:  nil,
+				},
+			},
+			want:    twoNode,
+			wantErr: false,
+		},
+		{
+			name: "Errors when the node is a leaf",
+			args: args{
+				node: twoThreeNode[int]{
+					firstData:   new(int),
+					secondData:  nil,
+					firstChild:  nil,
+					secondChild: nil,
+					thirdChild:  nil,
+				},
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "Errors in all other cases",
+			args: args{
+				node: twoThreeNode[int]{
+					firstData:   new(int),
+					secondData:  nil,
+					firstChild:  nil,
+					secondChild: &twoThreeNode[int]{},
+					thirdChild:  nil,
+				},
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := nodeType(tt.args.node)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("nodeType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("nodeType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (node *twoThreeNode[int]) setFirstData(i int) *twoThreeNode[int] {
+	node.firstData = &i
+	return node
+}
+
+func (node *twoThreeNode[int]) setSecondData(i int) *twoThreeNode[int] {
+	node.secondData = &i
+	return node
+}
+
+func (node *twoThreeNode[int]) setFirstChild(child *twoThreeNode[int]) *twoThreeNode[int] {
+	node.firstChild = child
+	return node
+}
+
+func (node *twoThreeNode[int]) setSecondChild(child *twoThreeNode[int]) *twoThreeNode[int] {
+	node.secondChild = child
+	return node
+}
+
+func (node *twoThreeNode[int]) setThirdChild(child *twoThreeNode[int]) *twoThreeNode[int] {
+	node.thirdChild = child
+	return node
+}
+
+func twoThreeNodeInt() *twoThreeNode[int] {
+	return &twoThreeNode[int]{
+		comparator: intComparator,
+	}
+}
+
+func (node *twoThreeNode[string]) equals(other *twoThreeNode[string]) bool {
+	return node.firstData == other.firstData &&
+		node.secondData == other.secondData &&
+		node.firstChild == other.firstChild &&
+		node.secondChild == other.secondChild &&
+		node.thirdChild == other.thirdChild
+}
+
+func (node *twoThreeNode[int]) toString() string {
+	var firstData int
+	var secondData int
+	if node.firstData != nil {
+		firstData = *node.firstData
+	}
+	if node.secondData != nil {
+		secondData = *node.secondData
+	}
+
+	return fmt.Sprintf("TwoThreeNode: [%v, %v]", firstData, secondData)
+}
+
+func Test_findLeaf(t *testing.T) {
+	type args struct {
+		value int
+	}
+
+	leafWithNine := twoThreeNodeInt().setFirstData(9)
+	leafWithSix := twoThreeNodeInt().setFirstData(6)
+	leafWithTwelve := twoThreeNodeInt().setFirstData(12)
+	leafWithOneAndThree := twoThreeNodeInt().setFirstData(1).setSecondData(3)
+
+	threeNodeWithSevenAndEleven := twoThreeNodeInt()
+	threeNodeWithSevenAndEleven.setFirstData(7).setSecondData(11)
+	threeNodeWithSevenAndEleven.setFirstChild(leafWithSix).setSecondChild(leafWithNine).setThirdChild(leafWithTwelve)
+
+	tree := twoThreeNodeInt().
+		setFirstData(4)
+	tree.setFirstChild(leafWithOneAndThree).
+		setSecondChild(threeNodeWithSevenAndEleven)
+
+	tests := []struct {
+		name string
+		args args
+		want *twoThreeNode[int]
+	}{
+		{
+			name: "Returns the correct leaf",
+			args: args{
+				value: 10,
+			},
+			want: leafWithNine,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, err := findLeaf(*tree, tt.args.value); !(got.equals(tt.want)) {
+				t.Errorf("findLeaf() = %v, want %v. Error: %v", got.toString(), tt.want.toString(), err)
 			}
 		})
 	}
